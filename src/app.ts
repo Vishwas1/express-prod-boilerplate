622c23ae8fdcc957e3a9a750d02d2efba87df473
vishwas1 <vishwasanand.cse@gmail.com>
2021-03-23 00:54:56 +0530

    adde rate limimt, limit

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,22 @@ import swaggerJsDoc = require('./swagger.json');
 import swaggerUi from 'swagger-ui-express';
 import { PORT, baseUrl } from './config';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import xss from 'xss-clean';
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests' // message to send
+  });
+  
+
 app.use(helmet())
-app.use(express.json());
+app.use(limiter)
+app.use(xss())
+app.use(express.json({ limit: '10kb' }));
 
 app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc))
 app.use('/api/v1/emp', routes.employee);
